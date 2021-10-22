263a0a16a289027d5415a11a3c0b41f56344d0eb
Komichi Sodai <83051052+Aso2101122@users.noreply.github.com>
2021-10-22 12:11:30 +0900

    Add files via upload

---
+++ rpgcreature/Braver.java
@@ -0,0 +1,57 @@
+package rpgcreature;
+
+import java.util.Random;
+
+/**
+ * 勇者クラス
+ */
+public class Braver extends Creature{
+    private final static int MAX_HP = 100;
+    private final static int RECOVERY_POINT = 20;
+    private final int NORMAL_DAMAGE_LIMIT = 10;
+    private final int NORMAL_DAMEGE_lOWER_LIMIT = 1;
+    private final static int CRITICAL_HIT_RATE = 10;
+    private final static int CRITICAL_HIT_DAMAGE = 50;
+
+    /**
+     * 勇者クラスのコンストラクタ
+     */
+    public Braver(String name){
+        super(name,MAX_HP);
+    }
+    
+    /**
+     * 攻撃メソッド
+     * @param opponent：攻撃相手
+     */
+    @Override
+    public void attack(Creature opponent){
+        Random r = new Random();
+        int damage = 0;
+        System.out.printf("%sの攻撃！\n",getName());
+
+        //クリティカルヒットかのチェック
+        if( r.nextInt(100) < CRITICAL_HIT_RATE ){
+            //クリティカルヒット
+            damage = CRITICAL_HIT_DAMAGE;
+            System.out.printf("%sのクリティカルヒット！\n",getName());
+        }else{
+            damage = r.nextInt(NORMAL_DAMAGE_LIMIT)+NORMAL_DAMEGE_lOWER_LIMIT;
+            
+        }
+        opponent.damaged(damage);
+        
+        displayMessage(opponent,damage);
+    }
+
+    /**
+     * HP回復処理
+     */
+    public void recovery(){
+        System.out.printf("%sはHPを回復した！\n",getName());
+        hp += RECOVERY_POINT;
+        if( hp > MAX_HP ){
+            hp = MAX_HP;
+        }
+    }
+}
